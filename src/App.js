208d09b08559fd61418588941ed611e7e4fc0fd0
kingearl123 <Sutejabatin@gmail.com>
2024-03-14 07:49:00 +0800

    main

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import Login from "./components/login";
+// import Register from "./components/register";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle";
+import Homepage from "./view/Homepage";
+import Login from "./view/login";
+import Register from "./view/register";
+import Input from "./view/pages/input";
+import Show from "./view/pages/show";
+import Edit from "./view/pages/edit";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Homepage />} />{" "}
+        <Route path="/register" element={<Register />} />{" "}
+        <Route path="/login" element={<Login />} />{" "}
+        <Route path="/input" element={<Input />} />{" "}
+        <Route path="/show" element={<Show />} />{" "}
+        <Route path="/edit/:id" element={<Edit />} />{" "}
+        {/* Tambahkan rute lainnya di sini jika diperlukan */}{" "}
+      </Routes>{" "}
+    </Router>
   );
 }
 
